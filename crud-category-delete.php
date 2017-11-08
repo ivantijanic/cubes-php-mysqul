d66a7bff7385138f1f79633748a1c739592dc404
Ivan Tijanic <it.tijanic@gmail.com>
2017-11-08 15:55:05 +0100

    Cas 28 kraj

--- crud-category-delete.php
+++ crud-category-delete.php
@@ -1,6 +1,20 @@
 <?php
 session_start();
 
+require_once __DIR__ . '/models/m_categories.php';
+
+if (empty($_GET['id'])) {
+    die('Morate proslediti id');
+}
+
+$id = (int) $_GET ['id'];
+
+$category = categoriesFetchOneById($id);
+
+if (empty($category)){
+    die ('Izabrali ste nepos,tojecu kategoriju');
+}
+
 require_once __DIR__ . '/views/layout/header.php';
 require_once __DIR__ . '/views/templates/t_crud-category-delete.php';
 require_once __DIR__ . '/views/layout/footer.php';
--- crud-category-list.php
+++ crud-category-list.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+require_once __DIR__ . '/models/m_categories.php';
+
+$categories = categoriesFetchAll();
+
 require_once __DIR__ . '/views/layout/header.php';
 require_once __DIR__ . '/views/templates/t_crud-category-list.php';
 require_once __DIR__ . '/views/layout/footer.php';
